0f4fec2c980230b60561191280fc0c5f4c298c27
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-11-28 18:19:58 +0100

    Added TaskListComponent to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import GreetingF from './components/pure/greetingF.jsx'
+import TaskListComponent from './components/container/task_list';
+//import GreetingF from './components/pure/greetingF.jsx'
+//import Greeting from './components/pure/greeting.jsx'
+
 
 function App() {
   return (
     <div className="App">
       <header classNam e="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <GreetingF name = "Javier"></GreetingF>
+
+        {/*Componente de ejemplo funcional*/}
+        {/*<Greeting name = "Javier"></Greeting>*/}
+        {/*<GreetingF name = "Javier"></GreetingF>*/}
+
+        {/*Componente de Listado de tareas*/}
+        <TaskListComponent></TaskListComponent>
       </header>
     </div>
   );
