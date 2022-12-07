62c78486031582632ee561aa1e6d84eed69e05e5
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-12-07 13:36:24 +0100

    Making proves with hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import TaskListComponent from './components/container/task_list';
+//import TaskListComponent from './components/container/task_list';
+//import Ejemplo1 from './hooks/ejemplo1.jsx'
+//import Ejemplo2 from './hooks/ejemplo2';
+import Ejemplo4 from './hooks/ejemplo4'
+import MiComponenteConContexto from './hooks/ejemplo3.jsx';
 //import GreetingF from './components/pure/greetingF.jsx'
 //import Greeting from './components/pure/greeting.jsx'
 
@@ -16,8 +20,19 @@ function App() {
         {/*<GreetingF name = "Javier"></GreetingF>*/}
 
         {/*Componente de Listado de tareas*/}
-        <TaskListComponent></TaskListComponent>
-      </header>
+        {/*<TaskListComponent></TaskListComponent>*/}
+
+        {/*Ejemplo de uso de HOOKS*/}
+        {/*<Ejemplo1></Ejemplo1>*/}
+        {/*<Ejemplo2></Ejemplo2*/}
+        <MiComponenteConContexto></MiComponenteConContexto>
+        <Ejemplo4 nombre = 'Javier'>
+        {/* Todo lo que haya aquí es tratado como props.children */}
+          <h3>
+            contenido del props.children
+          </h3>
+        </Ejemplo4>
+        </header>
     </div>
   );
 }
